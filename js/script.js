6959d87da3b3d7b12e91f887e81cefa3f5005a26
SydVic <fabrizio.bocca27@gmail.com>
2022-04-06 13:24:45 +0200

    aggiunto script password

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// raccolta dati forniti dall'utente
+const userName = prompt("Qual'è il tuo nome?");
+console.log(userName, typeof(userName));
+
+const userSurname = prompt("Qual'è il tuo cognome?");
+console.log(userSurname, typeof(userSurname));
+
+const userFavoriteColour = prompt("E il tuo colore preferito?");
+console.log(userFavoriteColour, typeof(userFavoriteColour));
+
+// concatenazione dei dati forniti
+const userPassword= `La tua password è: ${userName}${userSurname}${userFavoriteColour}21`;
+
+// inserimento stringa in HTML
+document.getElementById('user-password').innerHTML = userPassword;
